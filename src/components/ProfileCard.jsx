a69db31f8cad7ec5953e608d6e5fdb71ba3938a9
ghettoarcher <adnan30062000@gmail.com>
2024-08-28 14:41:18 +0200

    add props to ProfileCard

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,11 +1,11 @@
-const ProfileCard = () =>{
+const ProfileCard = ({name,occupation,articlesNum,avatar}) =>{
       return(
             <div className="pt-9  border-2 rounded-xl absolute right-44 bg-white">
                   <div className="px-12 flex flex-col items-center justify-center">
-                        <img className="w-20 h-20" src="/src/assets/img/image-220.svg" alt="avatar" />
-                        <div className="text-xl font-bold pt-4">Mira Anderson</div>
-                        <div className="text-sm pt-1">Traveler & Photographer</div>
-                        <di className="text-sm font-semibold pt-4">82 articles</di>
+                        <img className="w-20 h-20" src={avatar} alt="avatar" />
+                        <div className="text-xl font-bold pt-4">{name}</div>
+                        <div className="text-sm pt-1">{occupation}</div>
+                        <di className="text-sm font-semibold pt-4">{articlesNum} articles</di>
                         <div className="flex flex-row gap-2 pt-6 pb-8">
                               <div><img src="/src/assets/icons/facebook-white.svg" alt="" /></div>
                               <div><img src="/src/assets/icons/twitter-white.svg" alt="" /></div>
