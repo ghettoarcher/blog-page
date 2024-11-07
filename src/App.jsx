a79cf49c4ec96005bd2962b3799bd3e24e01c98e
AdnanAlezari <adnan30062000@gmail.com>
2024-11-07 22:41:30 +0100

    fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import Home from "./components/Home"
-import { BrowserRouter,Routes,Route } from "react-router-dom"
-import ArticlePage from "./Pages/ArticlePage"
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import SignIn from "./components/SignIn/SignIn";
+import Home from "./components/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ArticlePage from "./Pages/ArticlePage";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/post/:title" element={<ArticlePage />}/>
-        <Route path="sign-in" element={<SignIn />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/post/:title" element={<ArticlePage />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
